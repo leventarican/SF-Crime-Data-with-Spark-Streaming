13a31184043118877bf3da043156d993123001a8
leventarican <levent@protonmail.com>
2020-09-21 14:46:19 +0200

    json producer and consumer

--- project/consumer_server.py
+++ project/consumer_server.py
@@ -1,9 +1,26 @@
-# OPTIONAL
 # created separately to check if your kafka_server.py is working properly.
-# TODO
+# make a screenshot
+# https://kafka-python.readthedocs.io/en/master/apidoc/KafkaConsumer.html#kafka.KafkaConsumer
 
-def app():
-    pass
+from kafka import KafkaConsumer
+import json
+
+class ConsumerServer(KafkaConsumer):
+    def __init_(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def run(self):
+        for msg in self:
+            print(msg.value)
 
 if __name__ == "__main__":
-    app()
+    server = ConsumerServer(
+        'com.github.leventarican.foobar-0',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='the-consumer-group',
+        # Deserialize s (a str, bytes or bytearray instance containing a JSON document) to a Python object.
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    )
+    server.run()
--- project/producer_server.py
+++ project/producer_server.py
@@ -6,24 +6,31 @@ import time
 # You Need to Edit in Your Project Work
 #
 
+# https://kafka-python.readthedocs.io/en/master/apidoc/KafkaProducer.html
+
+# kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic com.github.leventarican.data --from-beginning
+
 class ProducerServer(KafkaProducer):
 
     def __init__(self, input_file, topic, **kwargs):
         super().__init__(**kwargs)
         self.input_file = input_file
         self.topic = topic
+        # self.valueserializer = lambda v: json.dumps(v).encode('utf-8')
 
     #TODO we're generating a dummy data
     def generate_data(self):
         with open(self.input_file) as f:
-            for line in f:
+            # Deserialize a file-like object containing a JSON document to a Python object (JSONDecoder)
+            obj = json.load(f)
+            for line in obj:
                 message = self.dict_to_binary(line)
-                print(message)
                 # TODO send the correct data
-                # self.send(value=message)
+                self.send(self.topic, value=message)
                 time.sleep(1)
 
     # TODO fill this in to return the json dictionary to binary
+    # Serialize obj to a JSON formatted str.
+    # encode will return bytes
     def dict_to_binary(self, json_dict):
-        return 
-        
+        return json.dumps(json_dict).encode('utf-8')
